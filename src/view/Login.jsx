8d9f6442defb5c392d5cae13be36bc4c285f4c83
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-07-08 18:23:46 -0300

    Login page

---
+++ src/view/Login.jsx
@@ -0,0 +1,54 @@
+const Login = () => {
+  return (
+    <div className="container-fluid d-flex justify-content-center align-items-center vh-100">
+      <form className="p-5 border rounded shadow-sm bg-light">
+        <div className="form-outline mb-4">
+          <input type="email" id="form2Example1" className="form-control" />
+          <label className="form-label" htmlFor="form2Example1">Email address</label>
+        </div>
+
+        <div className="form-outline mb-4">
+          <input type="password" id="form2Example2" className="form-control" />
+          <label className="form-label" htmlFor="form2Example2">Password</label>
+        </div>
+
+        <div className="row mb-4">
+          <div className="col d-flex align-items-center">
+            <div className="form-check">
+              <input className="form-check-input" type="checkbox" value="" id="form2Example31" checked />
+              <label className="form-check-label" htmlFor="form2Example31"> Remember me </label>
+            </div>
+          </div>
+
+          <div className="col text-end">
+            <a href="#!" className="text-decoration-none">Forgot password?</a>
+          </div>
+        </div>
+
+        <button type="button" className="btn btn-primary btn-block mb-4">Sign in</button>
+
+        <div className="text-center">
+          <p>Not a member? <a href="#!" className="text-decoration-none">Register</a></p>
+          <p>or sign up with:</p>
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-facebook-f"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-google"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-twitter"></i>
+          </button>
+
+          <button type="button" className="btn btn-link btn-floating mx-1">
+            <i className="fab fa-github"></i>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
