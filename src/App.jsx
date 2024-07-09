9d45b814538dd64afe0fa4e1aa84d285664cb5fb
Aldana <aldana_delgado@hotmail.com>
2024-07-09 19:24:01 -0300

    Agregada la landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ import HomePage from './view/HomePage';
 import Login from './view/Login';
 import Register from './view/Register';
 import ForgotPassword from './view/ForgotPassword';
+import LandingPage from './view/LandingPage';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Routes>
+          <Route path='/' element={<LandingPage/>}/>
           <Route path="/home" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
