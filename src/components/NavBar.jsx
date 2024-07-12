b8afefea3fedb0d5aacfc7d55bf1005ba47cea73
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-07-12 06:58:18 -0300

    Dark mode state

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,104 +1,118 @@
-import styles from '../utils/styles/LandingPage.module.css'
-import { useState, useEffect } from 'react'
-import SearchBar from './SearchBar'
-
-const darkMode = () => {
-	document.querySelector('html').setAttribute('data-bs-theme', 'dark')
-	document.querySelector('#dl-icon').className = 'bi bi-sun-fill'
-}
-
-const lightMode = () => {
-	document.querySelector('html').setAttribute('data-bs-theme', 'light')
-	document.querySelector('#dl-icon').className = 'bi bi-moon-fill'
-}
-
-const changeTheme = () => {
-	document.querySelector('html').getAttribute('data-bs-theme') === 'light'
-		? darkMode()
-		: lightMode()
-}
+import styles from '../utils/styles/LandingPage.module.css';
+import { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setDarkMode, setLightMode } from '../redux/actions';
+import SearchBar from './SearchBar';
 
 const NavBar = () => {
-	const [navbar, setNavbar] = useState(false)
+    const dispatch = useDispatch();
+    const theme = useSelector(state => state.theme.theme);
+    const [navbar, setNavbar] = useState(false);
 
-	const changeBackground = () => {
-		if (window.scrollY >= 80) {
-			setNavbar(true)
-		} else {
-			setNavbar(false)
-		}
-	}
+    const darkMode = () => {
+        document.querySelector('html').setAttribute('data-bs-theme', 'dark');
+        document.querySelector('#dl-icon').className = 'bi bi-sun-fill';
+    };
 
-	useEffect(() => {
-		window.addEventListener('scroll', changeBackground)
-		return () => {
-			window.removeEventListener('scroll', changeBackground)
-		}
-	}, [])
+    const lightMode = () => {
+        document.querySelector('html').setAttribute('data-bs-theme', 'light');
+        document.querySelector('#dl-icon').className = 'bi bi-moon-fill';
+    };
 
-	return (
-		<nav
-			className={`navbar navbar-expand-lg fixed-top ${
-				navbar ? styles.bgDark : styles.bgTransparent
-			}`}
-		>
-			<div className="container-fluid">
-				<a className="navbar-brand  " href="/">
-					ForDevs
-				</a>
+    const changeTheme = () => {
+        if (theme === 'light') {
+            dispatch(setDarkMode());
+        } else {
+            dispatch(setLightMode());
+        }
+    };
 
-				<button
-					className="navbar-toggler"
-					type="button"
-					data-bs-toggle="collapse"
-					data-bs-target="#navbarNav"
-					aria-controls="navbarNav"
-					aria-expanded="false"
-					aria-label="Toggle navigation"
-				>
-					<span className="navbar-toggler-icon"></span>
-				</button>
+    useEffect(() => {
+        if (theme === 'dark') {
+            darkMode();
+        } else {
+            lightMode();
+        }
+    }, [theme]);
 
-				<div className="collapse navbar-collapse" id="navbarNav">
-					<ul className="navbar-nav ">
-						<li className="nav-item ">
-							<a className="nav-link active " aria-current="page" href="/home">
-								Explorar
-							</a>
-						</li>
-						<li className="nav-item ">
-							<a className="nav-link" href="#">
-								Precios
-							</a>
-						</li>{' '}
-						<li className="nav-item ">
-							<a className="nav-link disabled " href="#">
-								Mis Proyectos
-							</a>
-						</li>
-					</ul>
-					<SearchBar />
-					<ul className="navbar-nav ms-auto">
-						<li className="nav-item">
-							<a className="nav-link " href="/login">
-								Ingresar
-							</a>
-						</li>
-						<li className="nav-item">
-							<a className="nav-link" href="/register">
-								Regístrate
-							</a>
-						</li>
-						<li className="nav-item">
-							<button onClick={changeTheme} className="btn rounded-fill">
-								<i id="dl-icon" className="bi bi-moon-fill"></i>
-							</button>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-	)
-}
+    const changeBackground = () => {
+        if (window.scrollY >= 80) {
+            setNavbar(true);
+        } else {
+            setNavbar(false);
+        }
+    };
 
-export default NavBar
+    useEffect(() => {
+        window.addEventListener('scroll', changeBackground);
+        return () => {
+            window.removeEventListener('scroll', changeBackground);
+        };
+    }, []);
+
+    return (
+        <nav
+            className={`navbar navbar-expand-lg fixed-top ${
+                navbar ? styles.bgDark : styles.bgTransparent
+            }`}
+        >
+            <div className="container-fluid">
+                <a className="navbar-brand" href="/">
+                    ForDevs
+                </a>
+
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarNav"
+                    aria-controls="navbarNav"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <a className="nav-link active" aria-current="page" href="/home">
+                                Explorar
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">
+                                Precios
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">
+                                Mis Proyectos
+                            </a>
+                        </li>
+                    </ul>
+                    <SearchBar />
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <a className="nav-link" href="/login">
+                                Ingresar
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/register">
+                                Regístrate
+                            </a>
+                        </li>
+                        <li className="nav-item">
+                            <button onClick={changeTheme} className="btn rounded-fill">
+                                <i id="dl-icon" className="bi bi-moon-fill"></i>
+                            </button>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,7 +1,5 @@
 import axios from 'axios';
-
-export const SET_AUTH_TOKEN = 'SET_AUTH_TOKEN';
-export const LOGOUT = 'LOGOUT';
+import { SET_DARK_MODE, SET_LIGHT_MODE, SET_AUTH_TOKEN, LOGOUT } from './actions-types';
 
 export const setAuthToken = (token) => ({
     type: SET_AUTH_TOKEN,
@@ -14,16 +12,20 @@ export const logout = () => ({
 
 export const loginUser = (email, password) => async (dispatch) => {
     try {
-        const response = await axios.post('http://localhost:3001/login', {
-            email,
-            password
-        });
+        const response = await axios.post('http://localhost:3001/login', { email, password });
         const token = response.data.token;
         dispatch(setAuthToken(token));
-        localStorage.setItem('authToken', token); // Guardar el token en localStorage para persistencia
         return { success: true };
     } catch (error) {
-        console.error('Error during login:', error);
+        console.error('Error during login:', error.response?.data || error.message);
         return { success: false, message: 'Credenciales inválidas' };
     }
 };
+
+export const setDarkMode = () => ({
+    type: SET_DARK_MODE
+});
+
+export const setLightMode = () => ({
+    type: SET_LIGHT_MODE
+});
