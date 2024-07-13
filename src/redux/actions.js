f7e1da25ec6b0d6523b3cd336c17fa838eef11e7
gastonibarra233 <ibarra.gl.2017@gmail.com>
2024-07-12 21:14:35 -0300

    filtros y cards renderizadas

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,138 +1,187 @@
-import axios from 'axios';
-import { SET_DARK_MODE, SET_LIGHT_MODE, SET_AUTH_TOKEN, LOGOUT, GET_ALL_PROJECTS, REGISTER_USER_FAILURE, REGISTER_USER_SUCCESS, REGISTER_USER_REQUEST, GET_USERS, GET_BY_NAME, GET_DETAIL, CLEAR_DETAIL  } from './actions-types';
+import axios from 'axios'
+import {
+	GET_ALL_PROJECTS,
+	SET_DARK_MODE,
+	SET_LIGHT_MODE,
+	SET_AUTH_TOKEN,
+	LOGOUT,
+	REGISTER_USER_FAILURE,
+	REGISTER_USER_SUCCESS,
+	REGISTER_USER_REQUEST,
+	GET_USERS,
+	GET_BY_NAME,
+	GET_DETAIL,
+	CLEAR_DETAIL,
+	FETCH_TECHNOLOGIES,
+	FILTER_TECHNOLOGIES,
+} from './actions-types'
 
-export const setAuthToken = (token) => ({
-    type: SET_AUTH_TOKEN,
-    payload: token
-});
+// const AUTH_URL = "http://localhost:3001/";
+// const USERS_URL = "http://localhost:3001/users";
+const PROJECTS_URL = 'http://localhost:3001/projects'
+const URL_TECHNOLOGIES = 'http://localhost:3001/technologies'
 
-export const logout = () => ({
-    type: LOGOUT
-});
-
-export const loginUser = (email, password) => async (dispatch) => {
-    try {
-        const response = await axios.post('http://localhost:3001/login', { email, password });
-        const token = response.data.token;
-        dispatch(setAuthToken(token));
-        return { success: true };
-    } catch (error) {
-        console.error('Error during login:', error.response?.data || error.message);
-        return { success: false, message: 'Credenciales inválidas' };
-    }
-};
-
-export const setDarkMode = () => ({
-    type: SET_DARK_MODE
-});
-
-export const setLightMode = () => ({
-    type: SET_LIGHT_MODE
-});
-
-export const getAllProjects = () => {
-    return async(dispatch) => {
-        try{
-            const response = await axios.get('http://localhost:3001/projects');
-            return dispatch({
-                type: GET_ALL_PROJECTS,
-                payload: response
-            });
-        } catch(error){
-            console.log("AAAAAA NOOOOO");
-            return distpatch({
-                type: Error,
-                payload: error.message
-            })
-        }
-    }
-};
-  export const registerUser = (userData) => async (dispatch) => {
-    dispatch(registerUserRequest());
-    try {
-      const response = await axios.post('http://localhost:3001/signup', userData);
-      dispatch(registerUserSuccess(response.data));
-    } catch (error) {
-      dispatch(registerUserFailure(error.message));
-    }
-  };
-  
-  const registerUserRequest = () => ({
-    type: REGISTER_USER_REQUEST,
-  });
-  
-  const registerUserSuccess = (data) => ({
-    type: REGISTER_USER_SUCCESS,
-    payload: data,
-  });
-  
-  const registerUserFailure = (error) => ({
-    type: REGISTER_USER_FAILURE,
-    payload: error,
-  });
-  export function getUsers(){
-    return async function(dispatch){
-        try {
-            const response = await axios.get(`http://localhost:3001/users/`);
-            
-            dispatch({
-                type: GET_USERS,
-                payload: response.data
-            });
-        } catch (error) {
-            console.error("Error fetching users:", error);
-        }
-    }
-};
-
-export function getByName(name){
-    return async function(dispatch){
-        try {
-            const response = await axios.get(`http://localhost:3001/users/?name=${name}`);
-            
-            dispatch({
-                type: GET_BY_NAME,
-                payload: response.data
-            });
-        } catch (error) {
-            console.error("Error fetching users by name:", error);
-            alert(`Error: ${error.message}`);
-        }
-    }
+export const fetchTechnologies = (token) => {
+	return async (dispatch) => {
+		try {
+			const { data } = await axios.get(`${URL_TECHNOLOGIES}`, {
+				headers: {
+					Authorization: `Bearer ${token.authToken}`,
+				},
+			})
+			dispatch({
+				type: FETCH_TECHNOLOGIES,
+				payload: data,
+			})
+		} catch (error) {
+			console.error('Error fetching technologies:', error)
+		}
+	}
 }
 
-export function getDetail(id){
-    return async function(dispatch){
-        try {
-            const response = await axios.get(`http://localhost:3001/users/${id}`);
-            
-            dispatch({
-                type: GET_DETAIL,
-                payload: response.data
-            });
-        } catch (error) {
-            console.error(`Error fetching users details for ID ${id}:`, error);
-        }
-    }
-};
+export const filterTechnologies = (filteredTechnologies) => {
+	return (dispatch) => {
+		dispatch({
+			type: FILTER_TECHNOLOGIES,
+			payload: filteredTechnologies,
+		})
+	}
+}
 
-export function clearDetail(){
-    return{
-        type: CLEAR_DETAIL
-    }
-};
+export const getAllProjects = (search, technologies) => {
+	return async (dispatch) => {
+		try {
+			let projects
+			if (!search && !technologies) projects = await axios.get(`${PROJECTS_URL}`)
+			if (search && !technologies)
+				projects = await axios.get(`${PROJECTS_URL}?search=${search}`)
+			if (!search && technologies)
+				projects = await axios.get(`
+					${PROJECTS_URL}?technologies=${technologies}`
+				)
+			if (search && technologies)
+				projects = await axios.get(
+					`${PROJECTS_URL}?search=${search}&technologies=${technologies}`
+				)
+			dispatch({
+				type: GET_ALL_PROJECTS,
+				payload: projects.data,
+			})
+			dispatch({
+				type: FILTER_TECHNOLOGIES,
+				payload: technologies,
+			})
+		} catch (error) {
+			throw error.message
+		}
+	}
+}
 
-export function getProjects() {
-  return async function (dispatch) {
-      try {
-          const response = await axios.get(`http://localhost:3001/projects`);
-          
-          dispatch({
-              type: GET_PROJECTS,
-              payload: response.data
-          });
-      } catch (error) {
-          console.error("Error fetching projects:", error);
-      }
-  };
+export const setAuthToken = (token) => ({
+	type: SET_AUTH_TOKEN,
+	payload: token,
+})
+
+export const logout = () => ({
+	type: LOGOUT,
+})
+
+export const loginUser = (email, password) => async (dispatch) => {
+	try {
+		const response = await axios.post('http://localhost:3001/', {
+			email,
+			password,
+		})
+		const token = response.data.token
+		dispatch(setAuthToken(token))
+		return { success: true }
+	} catch (error) {
+		console.error('Error during login:', error.response?.data || error.message)
+		return { success: false, message: 'Credenciales inválidas' }
+	}
+}
+
+export const setDarkMode = () => ({
+	type: SET_DARK_MODE,
+})
+
+export const setLightMode = () => ({
+	type: SET_LIGHT_MODE,
+})
+
+export const registerUser = (userData) => async (dispatch) => {
+	dispatch(registerUserRequest())
+	try {
+		const response = await axios.post('http://localhost:3001/signup', userData)
+		dispatch(registerUserSuccess(response.data))
+	} catch (error) {
+		dispatch(registerUserFailure(error.message))
+	}
+}
+
+const registerUserRequest = () => ({
+	type: REGISTER_USER_REQUEST,
+})
+
+const registerUserSuccess = (data) => ({
+	type: REGISTER_USER_SUCCESS,
+	payload: data,
+})
+
+const registerUserFailure = (error) => ({
+	type: REGISTER_USER_FAILURE,
+	payload: error,
+})
+export function getUsers() {
+	return async function (dispatch) {
+		try {
+			const response = await axios.get(`http://localhost:3001/users/`)
+
+			dispatch({
+				type: GET_USERS,
+				payload: response.data,
+			})
+		} catch (error) {
+			console.error('Error fetching users:', error)
+		}
+	}
+}
+
+export function getByName(name) {
+	return async function (dispatch) {
+		try {
+			const response = await axios.get(
+				`http://localhost:3001/users/?name=${name}`
+			)
+
+			dispatch({
+				type: GET_BY_NAME,
+				payload: response.data,
+			})
+		} catch (error) {
+			console.error('Error fetching users by name:', error)
+			alert(`Error: ${error.message}`)
+		}
+	}
+}
+
+export function getDetail(id) {
+	return async function (dispatch) {
+		try {
+			const response = await axios.get(`http://localhost:3001/users/${id}`)
+
+			dispatch({
+				type: GET_DETAIL,
+				payload: response.data,
+			})
+		} catch (error) {
+			console.error(`Error fetching users details for ID ${id}:`, error)
+		}
+	}
+}
+
+export function clearDetail() {
+	return {
+		type: CLEAR_DETAIL,
+	}
 }
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,115 +1,168 @@
-import { combineReducers } from 'redux';
-import { SET_DARK_MODE, SET_LIGHT_MODE, SET_AUTH_TOKEN, LOGOUT, GET_ALL_PROJECTS, REGISTER_USER_FAILURE, REGISTER_USER_SUCCESS, REGISTER_USER_REQUEST , GET_PROJECTS, GET_BY_NAME, GET_DETAIL, GET_USERS, CLEAR_DETAIL} from './actions-types';
+import { combineReducers } from 'redux'
+import {
+	GET_ALL_PROJECTS,
+	GET_PROJECT_BY_ID,
+	SET_DARK_MODE,
+	SET_LIGHT_MODE,
+	SET_AUTH_TOKEN,
+	LOGOUT,
+	REGISTER_USER_FAILURE,
+	REGISTER_USER_SUCCESS,
+	REGISTER_USER_REQUEST,
+	FETCH_TECHNOLOGIES,
+	FILTER_TECHNOLOGIES,
+} from './actions-types'
 
-const initialThemeState = {
-    theme: localStorage.getItem('theme') || 'light'
-};
-
-const themeReducer = (state = initialThemeState, action) => {
-    switch (action.type) {
-        case SET_DARK_MODE:
-            localStorage.setItem('theme', 'dark');
-            return { ...state, theme: 'dark' };
-        case SET_LIGHT_MODE:
-            localStorage.setItem('theme', 'light');
-            return { ...state, theme: 'light' };
-        default:
-            return state;
-    }
-};
-
-const authInitialState = {
-    authToken: localStorage.getItem('authToken') || null
-};
-
-const authReducer = (state = authInitialState, action) => {
-    switch (action.type) {
-        case SET_AUTH_TOKEN:
-            localStorage.setItem('authToken', action.payload);
-            return { ...state, authToken: action.payload };
-        case LOGOUT:
-            localStorage.removeItem('authToken');
-            return { ...state, authToken: null };
-        default:
-            return state;
-    }
-};
-
-const projectsInitialState = {
-    getAllProjects: [],
+const technologiesInitialState = {
+	technologies: [],
+	filteredTechnologies: []
 }
 
-const projects = (state = projectsInitialState, action) => {
-    switch(action.type){
-        case GET_ALL_PROJECTS:
-            return {...state, getAllProjects: action.payload}
-        default:
-            return state;
-    }
+const technologiesReducer = (state = technologiesInitialState, action) => {
+	switch (action.type) {
+		case FETCH_TECHNOLOGIES:
+			return {
+				...state,
+				technologies: action.payload,
+			}
+		case FILTER_TECHNOLOGIES:
+			console.log(action.payload)
+			return {
+				...state,
+				filteredTechnologies: action.payload
+			}
+        
+		default:
+			return state
+	}
 }
 
 const initialRegisterState = {
-    loading: false,
-    user: null,
-    error: null,
-};
+	loading: false,
+	user: null,
+	error: null,
+}
+
+const initialThemeState = {
+	theme: localStorage.getItem('theme') || 'light',
+}
+
+const authInitialState = {
+	authToken: localStorage.getItem('authToken') || null,
+}
+
+const projectsInitialState = {
+	allProjects: [],
+	filteredTechnologies: [],
+	project: {},
+}
+
+const themeReducer = (state = initialThemeState, action) => {
+	switch (action.type) {
+		case SET_DARK_MODE:
+			localStorage.setItem('theme', 'dark')
+			return { ...state, theme: 'dark' }
+		case SET_LIGHT_MODE:
+			localStorage.setItem('theme', 'light')
+			return { ...state, theme: 'light' }
+		default:
+			return state
+	}
+}
+
+const authReducer = (state = authInitialState, action) => {
+	switch (action.type) {
+		case SET_AUTH_TOKEN:
+			localStorage.setItem('authToken', action.payload)
+			return { ...state, authToken: action.payload }
+		case LOGOUT:
+			localStorage.removeItem('authToken')
+			return { ...state, authToken: null }
+		default:
+			return state
+	}
+}
+
+const projectsReducer = (state = projectsInitialState, action) => {
+	switch (action.type) {
+		case GET_ALL_PROJECTS:
+			return {
+				...state,
+				allProjects: action.payload,
+			}
+
+		case FILTER_TECHNOLOGIES:
+			return {
+				...state,
+				filteredTechnologies: action.payload
+			}
+		
+		case GET_PROJECT_BY_ID:
+			return {
+				...state,
+				project: action.payload,
+			}
+
+		default:
+			return state
+	}
+}
 
 const registerReducer = (state = initialRegisterState, action) => {
-    switch (action.type) {
-        case REGISTER_USER_REQUEST:
-            return { ...state, loading: true };
-        case REGISTER_USER_SUCCESS:
-            return { ...state, loading: false, user: action.payload };
-        case REGISTER_USER_FAILURE:
-            return { ...state, loading: false, error: action.payload };
-        default:
-            return state;
-    }
-};
-const initialState = {
-    allUsers: [],
-    users: [],
-    userProjects: [],
-    userDetails: {},
-};
+	switch (action.type) {
+		case REGISTER_USER_REQUEST:
+			return { ...state, loading: true }
+		case REGISTER_USER_SUCCESS:
+			return { ...state, loading: false, user: action.payload }
+		case REGISTER_USER_FAILURE:
+			return { ...state, loading: false, error: action.payload }
+		default:
+			return state
+	}
+}
+// const initialState = {
+//     allUsers: [],
+//     users: [],
+//     userProjects: [],
+//     userDetails: {},
+// };
 
-const rootReducer = (state = initialState, action) => {
-    switch (action.type) {
-        case GET_USERS:
-            return {
-                ...state,
-                users: action.payload,
-                allUsers: action.payload
-            };
-        case GET_BY_NAME:
-            return {
-                ...state,
-                users: action.payload,
-            };
-        case GET_DETAIL:
-            return {
-                ...state,
-                userDetails: action.payload,
-            };
-        case CLEAR_DETAIL:
-            return {
-                ...state,
-                userDetails: {},
-            };
-        case GET_PROJECTS:
-            return {
-                ...state,
-                userProjects: action.payload,
-            };
-        default:
-            return state;
-    }
-};
+// const rootReducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case GET_USERS:
+//             return {
+//                 ...state,
+//                 users: action.payload,
+//                 allUsers: action.payload
+//             };
+//         case GET_BY_NAME:
+//             return {
+//                 ...state,
+//                 users: action.payload,
+//             };
+//         case GET_DETAIL:
+//             return {
+//                 ...state,
+//                 userDetails: action.payload,
+//             };
+//         case CLEAR_DETAIL:
+//             return {
+//                 ...state,
+//                 userDetails: {},
+//             };
+//         case GET_PROJECTS:
+//             return {
+//                 ...state,
+//                 userProjects: action.payload,
+//             };
+//         default:
+//             return state;
+//     }
+// };
 
 export default combineReducers({
-    auth: authReducer,
-    theme: themeReducer,
-    project: projects,
-    register: registerReducer,
-});
-
+	auth: authReducer,
+	theme: themeReducer,
+	project: projectsReducer,
+	technologies: technologiesReducer
+})
