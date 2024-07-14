35ff7448c5acce8a3414f4788275ec1664ee7e9c
Aldana <aldana_delgado@hotmail.com>
2024-07-14 01:40:32 -0300

    Actualizar LandingPage y añadir imágenes al carousel

--- src/view/LandingPage.jsx
+++ src/view/LandingPage.jsx
@@ -1,9 +1,21 @@
+import React, { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import styles from '../utils/styles/LandingPage.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCheck } from '@fortawesome/free-solid-svg-icons';
+import { faCheck, faMoneyBillWave, faPercent, faCommentsDollar, faBriefcase,faRepeat, faUserGroup } from '@fortawesome/free-solid-svg-icons';
+
+import image1 from '../assets/images/carousel/imagen1.png';
+import image2 from '../assets/images/carousel/imagen2.png';
+import image3 from '../assets/images/carousel/imagen3.png';
 
 function LandingPage() {
+	useEffect(() => {
+		const carousel = document.querySelector('#carouselExampleIndicators');
+		if (carousel) {
+			carousel.setAttribute('data-bs-interval', '5000');
+		}
+	}, []);
+
 	return (
 		<div>
 			<header className={`${styles.heroSection} d-flex align-items-center justify-content-center`}>
@@ -12,9 +24,9 @@ function LandingPage() {
 						<div className="col-md-6">
 							<h1 className="display-3">Potencia tu carrera con ForDevs Pro</h1>
 							<p className="lead">
-								0 % de tarifas de la plataforma, más funciones para que tu carrera creativa crezca y dar más visibilidad a tus proyectos.
-							</p>
-							<a href="#" className="btn btn-primary">
+								0 % de tarifas de la plataforma, más funciones para que tu carrera creativa crezca 
+								y dar más visibilidad a tus proyectos.</p>
+							<a href="register" className="btn btn-primary">
 								Probar versión gratuita
 							</a>
 						</div>
@@ -45,20 +57,24 @@ function LandingPage() {
 								<div className="carousel-inner">
 									<div className="carousel-item active">
 										<img
-											src="https://media.istockphoto.com/id/1416797815/es/foto/golden-n%C3%BAmero-uno.jpg?s=1024x1024&w=is&k=20&c=EoMLATi8644HCy3K5IQB_QNEG_rN5qvQN9jByd19L0s="
+											src={image1}
 											className="d-block w-100"
 											alt="..."
 										/>
 									</div>
 									<div className="carousel-item">
 										<img
-											src="https://www.shutterstock.com/shutterstock/photos/1890767737/display_1500/stock-photo--d-render-number-two-glowing-in-the-dark-pink-blue-neon-light-1890767737.jpg"
+											src={image2}
 											className="d-block w-100"
 											alt="..."
 										/>
 									</div>
 									<div className="carousel-item">
-										<img src="https://via.placeholder.com/600x400" className="d-block w-100" alt="..." />
+										<img
+											src={image3}
+											className="d-block w-100"
+											alt="..."
+										/>
 									</div>
 								</div>
 								<button
@@ -84,6 +100,53 @@ function LandingPage() {
 					</div>
 				</div>
 			</header>
+			<div className="container my-5">
+				<div className="row align-items-center">
+					<div className="col-md-6">
+						<h2>Muestra tu trabajo en tu propio sitio web</h2>
+						<FontAwesomeIcon icon={faBriefcase} /> Publica hasta 5 sitios.<p />
+						<FontAwesomeIcon icon={faRepeat} />Importa tu trabajo desde ForDevs al instante <p/>
+						<a href="register" className="btn btn-primary">
+								Probar versión gratuita
+							</a>
+					</div>
+					<div className="col-md-6">
+						<img src={image1} className="img-fluid" alt="Imagen adicional 1" />
+					</div>
+				</div>
+			</div>
+			<div className="container my-5">
+				<div className="row align-items-center">
+					<div className="col-md-6">
+						<h2>Gana más dinero en ForDevs</h2>
+						<FontAwesomeIcon icon={faMoneyBillWave}/>  0 % de tarifas de plataforma en ForDevs para ventas de recursos<p />
+						<FontAwesomeIcon icon={faPercent}/>   0 % de tarifa de la plataforma ForDevs para autónomos y sus clientes<p />
+						<FontAwesomeIcon icon={faCommentsDollar} /> 0 % de tarifas de plataforma en ForDevs para suscripciones<p />
+						<a href="register" className="btn btn-primary">
+								Probar versión gratuita
+							</a>
+					</div>
+					<div className="col-md-6">
+						<img src={image2} className="img-fluid" alt="Imagen adicional 2" />
+					</div>
+				</div>
+			</div>
+			<div className="container my-5">
+				<div className="row align-items-center">
+					<div className="col-md-6">
+						<h2>Testimonios</h2>
+						<FontAwesomeIcon icon={faUserGroup} />Descubre cómo nuestros servicios han transformado la experiencia de nuestros usuarios. 	
+						Sus testimonios reflejan el impacto positivo que hemos logrado juntos.< p/>
+						<a href="register" className="btn btn-primary">
+								Probar versión gratuita
+							</a>
+						
+					</div>
+					<div className="col-md-6">
+						<img src={image3} className="img-fluid" alt="Imagen adicional 3" />
+					</div>
+				</div>
+			</div>
 			<div className="text-center mt-5 mb-5">
 				<h1 className="display-4">Empezá la prueba gratuita de 7 días</h1>
 				<p className="lead">Lleva tus proyectos al siguiente nivel con ForDevs Pro.</p>
@@ -93,13 +156,11 @@ function LandingPage() {
 							<div className="col-md-6">
 								<div className="card">
 									<div className="card-body">
-										<h5 className="card-title">
-											ForDevs
-										</h5>
+										<h5 className="card-title">ForDevs</h5>
 										<p className="card-text">
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Publica tus proyectos<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Ofrece servicios autónomos<br />
-											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Envía  propuestas a clientes<br />
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Envía propuestas a clientes<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Vende tus recursos<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Márcate como disponible para trabajar<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Accede a nuestra bolsa de trabajo autónomo y a tiempo completo<br />
@@ -111,9 +172,7 @@ function LandingPage() {
 							<div className="col-md-6">
 								<div className="card">
 									<div className="card-body">
-										<h5 className="card-title">
-											ForDevs Pro
-										</h5>
+										<h5 className="card-title">ForDevs Pro</h5>
 										<p className="card-text">
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Publica tus proyectos<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Ofrece servicios como freelance<br />
@@ -121,15 +180,16 @@ function LandingPage() {
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Vende tus recursos<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Marca tu disponibilidad para trabajar<br />
 											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> 0% de tarifa de la plataforma ForDevs<br />
-											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Proyectos protegidos con contraseña<br />
-											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Proyectos solo con enlace<br />
-											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Insignia profesional<br />
-											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Secciones de perfil<br />
-										</p>
-										<a href="#" className="btn btn-primary">
-											Probar versión Pro
-										</a>
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Proyectos protegidos por el seguro de calidad ForDevs<br />
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Ganancias transferidas automáticamente<br />
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Cuota de inscripción 20% más barata<br />
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Tienes acceso a la bolsa de trabajo freelance<br />
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Accede a la bolsa de trabajo de tiempo completo<br />
+											<FontAwesomeIcon icon={faCheck} style={{ color: "#63E6BE" }} /> Realiza transacciones en ForDevs<br />
+											</p>
+											<a href="register" className="btn btn-primary">Probar versión gratuita</a>
 									</div>
+
 								</div>
 							</div>
 						</div>
@@ -137,7 +197,7 @@ function LandingPage() {
 				</section>
 			</div>
 		</div>
-	)
+	);
 }
 
 export default LandingPage;
