6cf65f5117d6a1065cf9119719bdc20580c1549d
MiguelLinares01 <milangel1010@gmail.com>
2024-07-14 16:56:47 -0400

    Creación del componente ModProject.jsx

--- src/view/ModProject.jsx
+++ src/view/ModProject.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux'
+import '../App.css';
+import { fetchTechnologies, filterTechnologies, getAllProjects } from '../redux/actions'
 
 
 const ModProject = () => {
+
+    const dispatch = useDispatch()
+    const { technologies } = useSelector((state) => state.technologies)
+    console.log(technologies)
+    
+    useEffect(() => {
+        dispatch(fetchTechnologies(localStorage))
+    }, [])
+
+
     return(
     <div style={{width:"50%"}} class="mx-auto mt-5">
         <form action="">
@@ -14,8 +27,15 @@ const ModProject = () => {
                 <input type="text" key="description" id='description' name='description' style={{backgroundColor:"#212529", border:"1pt solid #656768", width:"100%", fontSize:"21px"}} class='mb-3' /> <br />
             </div>
             <div>
-                <label htmlFor="" style={{color:"white", fontSize:"20px"}}>Tecnologías:</label><br />            
-                <input type="text" key="technologies" id='technologies' name='technologies' style={{backgroundColor:"#212529", border:"1pt solid #656768", width:"100%", fontSize:"21px"}} class='mb-3' /> <br />
+                <label htmlFor="" style={{color:"white", fontSize:"20px"}}>Tecnologías:</label><br />
+                <ul>
+                    {technologies.map((value, index)=>(
+                        <li key={index} style={{display:"inline-block", margin:"9px"}}>
+                            <input key={value} id={value} name={index} type="checkbox" value={value.name}/>
+                            <label style={{fontSize:"20px", fontWeight:"bold"}} class='.custom-checkbox'>{value.name}</label>
+                        </li>
+                    ))}
+                </ul>            
             </div>
             <div>
                 <label htmlFor="" style={{color:"white", fontSize:"20px"}}>Tags:</label><br />
