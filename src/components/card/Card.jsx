b9eb262d42920811a439033f698c2041b583cad7
Aldana <aldana_delgado@hotmail.com>
2024-07-15 19:05:04 -0300

    Card/Cards de Projects con estilos

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,28 +1,57 @@
-import 'bootstrap/dist/css/bootstrap.min.css'
-import { Link } from 'react-router-dom'
-import styled from 'styled-components'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const Card = (project) => {
-	return (
-		<CardDiv className="card">
-			<img src={project.image} className="card-img-top" alt={project.title} />
-			<div className="card-body">
-				<h3 className="card-title">{project.title}</h3>
-				<p className="card-text">
-					{project.technologies.map(tech => tech.name).join(' ')}
-				</p>
-				<p>{project.tags.join(' ')}</p>
-				<Link to={`/project/detail/${project.id}`} className="btn btn-primary">
-					View Detail
-				</Link>
-			</div>
-		</CardDiv>
-	)
+const Card = ({ id, title, technologies, image, tags }) => {
+return (
+    <CardDiv className="card" style={{ width: '18rem' }}>
+    <img
+        src={image}
+        className="card-img-top"
+        alt={title}
+        style={{
+        width: '100%',
+        height: '200px',
+        objectFit: 'cover',
+        objectPosition: 'center',
+        }}
+    />
+    <div className="card-body">
+        <h5 className="card-title text-center">{title}</h5>
+        <div className="technologies">
+        {technologies.map((tech, index) => (
+            <button key={index} type="button" className="btn btn-outline-secondary btn-sm disabled"
+			style={{ marginRight: 5 }}>
+            {tech.name}
+            </button>
+        ))}
+        </div>
+        <div className="tags">
+        {tags.map((tag, index) => (
+            <button key={index} type="button" className="btn btn-outline-primary btn-sm disabled"
+			style={{ marginRight: 5 }}>
+            {tag}
+            </button>
+        ))}
+        </div>
+    </div>
+    <div className="text-center mb-3"> 
+        <Link to={`/project/detail/${id}`} className="btn btn-primary">
+        View Detail
+        </Link>
+    </div>
+    </CardDiv>
+);
+};
+
+//? Styles
+const CardDiv = styled.div`
+display: flex;
+flex-direction: column;
+height: 100%;
+.card-body {
+    flex-grow: 1;
 }
+`;
 
-export default Card
-
-// ? Styles
-const CardDiv = styled.section`
-	/* width: 100%; */
-`
+export default Card;
--- src/components/cards/Cards.jsx
+++ src/components/cards/Cards.jsx
@@ -1,12 +1,12 @@
-import { useSelector } from 'react-redux'
 import styled from 'styled-components'
-import Card from '../cards/Cards'
+import { useSelector } from 'react-redux'
+import Card from '../card/Card'
 
 const Cards = () => {
 	const { allProjects } = useSelector((state) => state.project)
 
 	return (
-		<CardsDiv className="container-fluid ">
+		<CardsDiv className="container-fluid">
 			{allProjects.length ? (
 				allProjects.map((project) => (
 					<Card
@@ -28,13 +28,13 @@ const Cards = () => {
 
 export default Cards
 
-// ? Styles
+// Styles
 const CardsDiv = styled.section`
 	display: grid;
 	grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
 	gap: 20px;
 	margin: 50px 0;
-	padding: 10px;
+	padding: 10px 30px;
 	@media (max-width: 992px) {
 		grid-template-columns: 1fr;
 	}
