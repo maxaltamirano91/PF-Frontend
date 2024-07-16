80960142d028dee910a8c8571762dc9b484bbea1
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-07-15 22:06:47 -0300

    Create Button with Token

--- src/view/home-page/HomePage.jsx
+++ src/view/home-page/HomePage.jsx
@@ -4,30 +4,47 @@ import { getAllProjects } from '../../redux/actions'
 import Cards from '../../components/cards/Cards'
 
 const HomePage = () => {
-	const dispatch = useDispatch()
-	const { allProjects } = useSelector((state) => state.project)
-	const [renderingCards, setRenderingCards] = useState(5)
-	const [displayPagination, setDisplayPagination] = useState(true)
-	const handlePagination = () => {
-		if (allProjects.length >= renderingCards) {
-			setRenderingCards((prevCount) => prevCount + 5)
-			dispatch(getAllProjects(renderingCards))
-		} else setDisplayPagination(false)
-	}
-	useEffect(() => {
-		dispatch(getAllProjects(renderingCards))
-	}, [dispatch, renderingCards])
+    const dispatch = useDispatch()
+    const { allProjects } = useSelector((state) => state.project)
+    const [renderingCards, setRenderingCards] = useState(5)
+    const [displayPagination, setDisplayPagination] = useState(true)
+    const authToken = useSelector((state) => state.auth.authToken)
 
-	return (
-		<div>
-			<Cards paginationData={renderingCards} />
-			{displayPagination ? (
-				<button onClick={handlePagination}>Ver más</button>
-			) : (
-				<p>Estos son todos los proyectos</p>
-			)}
-		</div>
-	)
+    const handlePagination = () => {
+        if (allProjects.length >= renderingCards) {
+            setRenderingCards((prevCount) => prevCount + 5)
+        } else {
+            setDisplayPagination(false)
+        }
+    }
+
+    useEffect(() => {
+        dispatch(getAllProjects(renderingCards))
+    }, [dispatch, renderingCards])
+
+    return (
+        <div className="container mt-4">
+            <div className="row mb-3">
+                <div className="col text-end">
+                    {authToken && (
+                        <a className="btn btn-primary" href="/create">
+                            Crear proyecto!
+                        </a>
+                    )}
+                </div>
+            </div>
+            <Cards paginationData={renderingCards} />
+            {displayPagination ? (
+                <div className="text-center mt-3">
+                    <button className="btn btn-secondary" onClick={handlePagination}>
+                        Ver más
+                    </button>
+                </div>
+            ) : (
+                <p className="text-center mt-3">Estos son todos los proyectos</p>
+            )}
+        </div>
+    )
 }
 
 export default HomePage
