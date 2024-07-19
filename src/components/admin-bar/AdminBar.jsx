e27af441944f355d6b0145967e3abf09dc3a5385
Mika <test@Tests-MacBook-Pro.local>
2024-07-19 11:14:31 -0300

    fix: change to deploy url

--- src/components/admin-bar/AdminBar.jsx
+++ src/components/admin-bar/AdminBar.jsx
@@ -1,36 +1,60 @@
-import { useNavigate} from "react-router-dom"
-import "./Admin-bar.module.css"
-import { FRONT_URL_BASE_FULL } from "../../../auth0-config";
+import { useNavigate } from 'react-router-dom'
+import './Admin-bar.module.css'
+import {
+	// FRONT_URL_BASE_FULL,
+	DEPLOY_FRONT_URL_BASE_FULL,
+} from '../../../auth0-config'
 
+const AdminBar = () => {
+	let data
+	const navigate = useNavigate()
+	const adminViewU = () => {
+		const currentUrl = window.location.href
+		data = 'Users'
+		currentUrl === `${DEPLOY_FRONT_URL_BASE_FULL}/adminView/Users`
+			? null
+			: navigate(`/adminView/${data}`)
+	}
+	const adminViewP = () => {
+		const currentUrl = window.location.href
+		data = 'Projects'
+		currentUrl === `${DEPLOY_FRONT_URL_BASE_FULL}/adminView/Projects`
+			? null
+			: navigate(`/adminView/${data}`)
+	}
+	const adminViewT = () => {
+		const currentUrl = window.location.href
+		data = 'Technologies'
+		currentUrl === `${DEPLOY_FRONT_URL_BASE_FULL}/adminView/Technologies`
+			? null
+			: navigate(`/adminView/${data}`)
+	}
 
-const AdminBar = () =>{
-
-    let data;
-    const navigate = useNavigate()
-    const adminViewU = () => {
-        const currentUrl = window.location.href;
-        data = "Users"
-        currentUrl === `${FRONT_URL_BASE_FULL}/adminView/Users` ? null : navigate(`/adminView/${data}`)
-    }
-    const adminViewP = () => {
-        const currentUrl = window.location.href;
-        data = "Projects"
-        currentUrl === `${FRONT_URL_BASE_FULL}/adminView/Projects` ? null : navigate(`/adminView/${data}`)
-    }
-    const adminViewT = () => {
-        const currentUrl = window.location.href;
-        data = "Technologies"
-        currentUrl === `${FRONT_URL_BASE_FULL}/adminView/Technologies` ? null : navigate(`/adminView/${data}`)
-    }
-    
-
-    return(
-        <div style={{padding:"5px 20px"}}>
-            <button className='text-body' style={{margin:"0px 5px", textDecoration:"none"}} onClick={adminViewU}>Usuarios</button>
-            <button className='text-body' style={{margin:"0px 5px", textDecoration:"none"}} onClick={adminViewP}>Proyectos</button>
-            <button className='text-body' style={{margin:"0px 5px", textDecoration:"none"}} onClick={adminViewT}>Tecnologías</button>
-        </div>
-    )
+	return (
+		<div style={{ padding: '5px 20px' }}>
+			<button
+				className="text-body"
+				style={{ margin: '0px 5px', textDecoration: 'none' }}
+				onClick={adminViewU}
+			>
+				Usuarios
+			</button>
+			<button
+				className="text-body"
+				style={{ margin: '0px 5px', textDecoration: 'none' }}
+				onClick={adminViewP}
+			>
+				Proyectos
+			</button>
+			<button
+				className="text-body"
+				style={{ margin: '0px 5px', textDecoration: 'none' }}
+				onClick={adminViewT}
+			>
+				Tecnologías
+			</button>
+		</div>
+	)
 }
 
-export default AdminBar;
+export default AdminBar
