84c6da1971e75ba03c8158dc8a10d79649412079
ivanjavierbell@gmail.com <ivanjavierbell@gmail.com>
2024-07-23 16:25:24 -0300

    Order buttons not working

--- src/components/filter/Filter.jsx
+++ src/components/filter/Filter.jsx
@@ -1,49 +1,49 @@
-import styled from 'styled-components'
-import { useState, useEffect } from 'react'
-import Select from 'react-select'
-import { useDispatch, useSelector } from 'react-redux'
+import { useState, useEffect } from 'react';
+import Select from 'react-select';
+import { useDispatch, useSelector } from 'react-redux';
 import {
     fetchTechnologies,
     filterTechnologies,
     getAllProjects,
-} from '../../redux/actions'
+} from '../../redux/actions';
+import styles from './Filter.module.css';
 
 const Filter = () => {
-    const dispatch = useDispatch()
-    const { technologies } = useSelector((state) => state.technologies)
-    const { token } = useSelector((state) => state.auth)
-    const [selectedOptions, setSelectedOptions] = useState([])
-    const [search, setSearch] = useState('')
-    const [sortOrder, setSortOrder] = useState('new')
-    const pagination = 10
+    const dispatch = useDispatch();
+    const { technologies } = useSelector((state) => state.technologies);
+    const { token } = useSelector((state) => state.auth);
+    const [selectedOptions, setSelectedOptions] = useState([]);
+    const [search, setSearch] = useState('');
+    const [sortOrder, setSortOrder] = useState('new');
+    const pagination = 10;
 
     const options = technologies.map((tech) => ({
         value: tech.name,
         label: tech.name,
         key: tech.name,
-    }))
+    }));
 
     const handleInputChange = (select) => {
-        setSelectedOptions(select)
-    }
+        setSelectedOptions(select);
+    };
 
     const handleSortChange = (sort) => {
-        setSortOrder(sort)
-    }
+        setSortOrder(sort);
+    };
 
     const handleSubmit = () => {
-        const selectedTechnologies = selectedOptions.map((option) => option.value)
-        dispatch(filterTechnologies(selectedTechnologies))
-        dispatch(getAllProjects(pagination, search, selectedTechnologies, sortOrder))
-    }
+        const selectedTechnologies = selectedOptions.map((option) => option.value);
+        dispatch(filterTechnologies(selectedTechnologies));
+        dispatch(getAllProjects(pagination, search, selectedTechnologies, sortOrder));
+    };
 
     useEffect(() => {
-        dispatch(fetchTechnologies(token))
-    }, [dispatch, token])
+        dispatch(fetchTechnologies(token));
+    }, [dispatch, token]);
 
     return (
-        <SectionStyled className="">
-            <div className="">
+        <section className={styles.section}>
+            <div>
                 <link
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css"
@@ -52,7 +52,7 @@ const Filter = () => {
                     referrerPolicy="no-referrer"
                 />
             </div>
-            <div className="selects-container ">
+            <div className={styles.selectsContainer}>
                 <Select
                     options={options}
                     onChange={handleInputChange}
@@ -62,7 +62,7 @@ const Filter = () => {
                     styles={customStylesSelectReact}
                 />
             </div>
-            <div className="search-container ">
+            <div className={styles.searchContainer}>
                 <input
                     type="search"
                     value={search}
@@ -70,106 +70,42 @@ const Filter = () => {
                     placeholder="Nombre del proyecto"
                 />
             </div>
-            <div className="btn-container ">
+            <div className={styles.btnContainer}>
                 <button className="btn btn-light" onClick={handleSubmit}>
                     Filtrar
                 </button>
             </div>
-            <div className="sort-buttons">
+            <div className={styles.sortButtons}>
                 <button
-                    className={sortOrder === 'a-z' ? 'selected' : ''}
+                    className={sortOrder === 'a-z' ? styles.selected : ''}
                     onClick={() => handleSortChange('a-z')}
                 >
                     A-Z
                 </button>
                 <button
-                    className={sortOrder === 'z-a' ? 'selected' : ''}
+                    className={sortOrder === 'z-a' ? styles.selected : ''}
                     onClick={() => handleSortChange('z-a')}
                 >
                     Z-A
                 </button>
                 <button
-                    className={sortOrder === 'new' ? 'selected' : ''}
+                    className={sortOrder === 'new' ? styles.selected : ''}
                     onClick={() => handleSortChange('new')}
                 >
                     Recientes
                 </button>
                 <button
-                    className={sortOrder === 'old' ? 'selected' : ''}
+                    className={sortOrder === 'old' ? styles.selected : ''}
                     onClick={() => handleSortChange('old')}
                 >
                     Viejos
                 </button>
             </div>
-        </SectionStyled>
-    )
-}
+        </section>
+    );
+};
 
-export default Filter
-
-// ? Styles
-const SectionStyled = styled.section`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 1rem;
-    justify-content: center;
-    align-items: center;
-
-    .selects-container {
-        color-scheme: light;
-    }
-
-    .search-container {
-        input {
-            height: 39px;
-            border-radius: 6px;
-            border-color: #a8a8a88e;
-            color-scheme: light;
-            padding: 0 12px;
-            font-weight: 600;
-        }
-        :focus {
-            border-color: #0099ff;
-            outline: none;
-            border-width: 3px;
-        }
-        ::placeholder {
-            color: #a8a8a8;
-            font-weight: 400;
-        }
-    }
-
-    .btn-container {
-        button {
-            margin: 0;
-            border-color: #a8a8a88e;
-            color: gray;
-        }
-        &:hover {
-            text-decoration: none;
-        }
-    }
-
-    .sort-buttons {
-        button {
-            margin: 0 5px;
-            padding: 8px 16px;
-            border: 1px solid #a8a8a88e;
-            background-color: white;
-            color: gray;
-            cursor: pointer;
-            border-radius: 4px;
-        }
-        button.selected {
-            background-color: #0099ff;
-            color: white;
-        }
-        button:hover {
-            background-color: #007acc;
-            color: white;
-        }
-    }
-`
+export default Filter;
 
 const customStylesSelectReact = {
     control: (provided, state) => ({
@@ -196,4 +132,4 @@ const customStylesSelectReact = {
         ...provided,
         color: '#fff',
     }),
-}
+};
