39d7e043184b01bd1addb95d7f412ec9dbdb0423
Mika <test@Tests-MacBook-Pro.local>
2024-07-25 03:03:28 -0300

    fix: filter redesign

---
+++ src/components/filter/Selector.jsx
@@ -0,0 +1,54 @@
+import styles from './Filter.module.css'
+import { useDispatch, useSelector } from 'react-redux'
+import { fetchTechnologies } from '../../redux/actions'
+import { useEffect, useState } from 'react'
+import Select from 'react-select'
+
+const Selector = ({ onTechnologyChange, selectedTechnologies }) => {
+	const dispatch = useDispatch()
+	const { theme } = useSelector((state) => state.themes)
+	const { technologies } = useSelector((state) => state.technologies)
+	const [dropdownOpen, setDropdownOpen] = useState(false)
+
+	useEffect(() => {
+		dispatch(fetchTechnologies())
+	}, [dispatch])
+
+	const technologyOptions = technologies.map((tech) => ({
+		value: tech.name,
+		label: tech.name,
+		key: tech.name,
+	}))
+
+	const toggleDropdown = () => setDropdownOpen(!dropdownOpen)
+
+	return (
+		<div className={`${styles.selectContainer} dropdown`}>
+			<div
+				className="dropdown-toggle"
+				style={theme === 'dark' ? {} : { backgroundColor: '#f8f9fa' }}
+				aria-expanded={dropdownOpen}
+				onClick={toggleDropdown}
+			>
+				Filter
+			</div>
+			<div
+				className={`dropdown-menu ${dropdownOpen ? 'show' : ''}`}
+				aria-labelledby="dropdownMenuButton"
+			>
+				<div className="p-3">
+					<Select
+						options={technologyOptions}
+						onChange={onTechnologyChange}
+						value={selectedTechnologies}
+						isMulti={true}
+						placeholder="Selecciona tecnología"
+						classNamePrefix="react-select"
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Selector
---
+++ src/components/filter/Sort.jsx
@@ -0,0 +1,40 @@
+import styles from './Filter.module.css'
+
+const Sort = ({ sortOrder, onSortChange }) => {
+	return (
+		<div className={`${styles.sortContainer} dropdown`}>
+			<div
+				className="dropdown-toggle"
+				id="dropdownMenuButton"
+				data-bs-toggle="dropdown"
+				aria-expanded="false"
+			>
+				Sort by {sortOrder}
+			</div>
+			<ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+				<li>
+					<button className="dropdown-item" onClick={() => onSortChange('a-z')}>
+						De la A-Z
+					</button>
+				</li>
+				<li>
+					<button className="dropdown-item" onClick={() => onSortChange('z-a')}>
+						De la Z-A
+					</button>
+				</li>
+				<li>
+					<button className="dropdown-item" onClick={() => onSortChange('new')}>
+						Los más recientes
+					</button>
+				</li>
+				<li>
+					<button className="dropdown-item" onClick={() => onSortChange('old')}>
+						Los más antiguos
+					</button>
+				</li>
+			</ul>
+		</div>
+	)
+}
+
+export default Sort
