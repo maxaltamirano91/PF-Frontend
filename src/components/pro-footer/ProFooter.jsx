f59b3e6764be84f05e190911eef94a259b9f91ea
Mika <test@Tests-MacBook-Pro.local>
2024-07-31 17:20:08 -0300

    semi final commit

--- src/components/pro-footer/ProFooter.jsx
+++ src/components/pro-footer/ProFooter.jsx
@@ -3,7 +3,7 @@ import { useSelector } from 'react-redux'
 
 import { useLocation, Link } from 'react-router-dom'
 
-const ProFooter = ({loading}) => {
+const ProFooter = ({ loading }) => {
 	const { loggedUser } = useSelector((state) => state.auth)
 	const { pathname } = useLocation()
 
@@ -27,49 +27,68 @@ const ProFooter = ({loading}) => {
 
 	if (pathname === '/explorer/users') {
 		return (
-			<div>
-			{!loading ?
-			<div className={`${styles.proFooter} text-center mt-4`}>
-			<span>No se encontraron usuarios que coincidan con tu búsqueda.</span>
-				<span className="fw-bold">
-					Te recomendamos explorar estos otros temas:
-				</span>
-				<Link to="/home">Proyectos</Link>
-			</div>
-			:
-			<div className='d-flex justify-content-center mb-3'>
-				<div class="spinner-border text-primary" role="status">
-  					<span class="visually-hidden">Loading...</span>
-				</div>
-				<br /><br /><br />
-			</div>
-			}
-
+			<div className='mt-5'>
+				<br />
+				<br />
+				<br />
+				{!loading ? (
+					<div className={`${styles.proFooter} text-center mt-4`}>
+						<span>
+							No se encontraron usuarios que coincidan con tu búsqueda.
+						</span>
+						<span className="fw-bold">
+							Te recomendamos explorar estos otros temas:
+						</span>
+						<Link to="/home">Proyectos</Link>
+					</div>
+				) : (
+					<div className="d-flex justify-content-center mb-3">
+						<div className="spinner-border text-primary" role="status">
+							<span className="visually-hidden">Loading...</span>
+						</div>
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+					</div>
+				)}
 			</div>
 		)
 	}
 
 	if (pathname === '/home') {
 		return (
-			<div>
-
-			{!loading ?
-			<div className={`${styles.proFooter} text-center mt-4`}>
-			<span>No se encontraron proyectos que coincidan con tu búsqueda</span>
-				<span className="fw-bold">
-					Te recomendamos explorar estos otros temas:
-				</span>
-				<Link to="/explorer/users">Usuarios</Link>
-			</div>
-			:
-			<div className='d-flex justify-content-center mb-3'>
-				<div class="spinner-border text-primary" role="status">
-  					<span class="visually-hidden">Loading...</span>
-				</div>
-				<br /><br /><br />
-			</div>
-			}
-				
+			<div className='mt-5'>
+				<br />
+				<br />
+				<br />
+				{!loading ? (
+					<div className={`${styles.proFooter} text-center mt-4`}>
+						<span>
+							No se encontraron proyectos que coincidan con tu búsqueda
+						</span>
+						<span className="fw-bold">
+							Te recomendamos explorar estos otros temas:
+						</span>
+						<Link to="/explorer/users">Usuarios</Link>
+					</div>
+				) : (
+					<div className="d-flex justify-content-center mb-3">
+						<div className="spinner-border text-primary" role="status">
+							<span className="visually-hidden">Loading...</span>
+						</div>
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+						<br />
+					</div>
+				)}
 			</div>
 		)
 	}
--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -1,129 +1,124 @@
-import styles from './Tabs.module.css';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import Reviews from '../reviews/Reviews';
-import PropTypes from 'prop-types';
-import ContractView from '../../view/profile-page/components/ContractView';
-import ProfileProjects from '../../components/profile-projects/ProfileProjects';
+import styles from './Tabs.module.css'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { useSelector } from 'react-redux'
+import Reviews from '../reviews/Reviews'
+import PropTypes from 'prop-types'
+import ContractView from '../../view/profile-page/components/ContractView'
+import ProfileProjects from '../../components/profile-projects/ProfileProjects'
 
 const Tabs = ({
-  profileData,
-  onRestore,
-  deletedProjects,
-  searchQuery,
-  activeContractTab,
-  handleReviewFormSubmit,
-  handleDelete,
-  onClick, // Nueva prop para manejar clics en pestañas
-  activeTab, // Nueva prop para la pestaña activa
+	profileData,
+	onRestore,
+	deletedProjects,
+	searchQuery,
+	activeContractTab,
+	handleReviewFormSubmit,
+	handleDelete
 }) => {
-  const { loggedUser } = useSelector((state) => state.auth);
-  const categories = Object.freeze({
-    contracts: 'contracts',
-  });
+	const { loggedUser } = useSelector((state) => state.auth)
+	const { theme } = useSelector((state) => state.themes)
+	const [activeTab, setActiveTab] = useState('projects')
+	const categories = Object.freeze({
+		contracts: 'contracts',
+	})
 
-  const tabs = [
-    {
-      key: 'projects',
-      label: 'Proyectos',
-      content: (
-        <ProfileProjects tabName="Proyectos" profileData={profileData} />
-      ),
-    },
-    {
-      key: 'reviews',
-      label: 'Reviews',
-      content: (
-        <Reviews
-          profileData={profileData}
-          handleReviewFormSubmit={handleReviewFormSubmit}
-          handleDelete={handleDelete}
-        />
-      ),
-    },
-  ];
+	const tabs = [
+		{
+			key: 'projects',
+			label: 'Proyectos',
+			content: (
+				<ProfileProjects tabName="Proyectos" profileData={profileData} />
+			),
+		},
+		{
+			key: 'reviews',
+			label: 'Reviews',
+			content: (
+				<Reviews
+					profileData={profileData}
+					handleReviewFormSubmit={handleReviewFormSubmit}
+					handleDelete={handleDelete}
+				/>
+			),
+		},
+	]
 
-  // Solo agregar la pestaña de contratos si el usuario actual es el propietario del perfil
-  if (loggedUser?.id === profileData?.id) {
-    tabs.push({
-      key: 'contracts',
-      label: 'Contratos',
-      content: (
-        <ContractView
-          searchQuery={searchQuery}
-          activeContractTab={activeContractTab}
-          categories={categories}
-        />
-      ),
-    });
+	// Solo agregar la pestaña de contratos si el usuario actual es el propietario del perfil
+	if (loggedUser?.id === profileData?.id) {
+		tabs.push({
+			key: 'contracts',
+			label: 'Contratos',
+			content: (
+				<ContractView
+					searchQuery={searchQuery}
+					activeContractTab={activeContractTab}
+					categories={categories}
+				/>
+			),
+		})
 
-    // Agregar la pestaña de archivados solo si el usuario actual es el propietario del perfil
-    const modifiedProfileData = { ...profileData, projects: deletedProjects };
-    tabs.push({
-      key: 'archived',
-      label: 'Archivados',
-      content: (
-        <ProfileProjects
-          tabName="Proyectos Archivados"
-          profileData={modifiedProfileData}
-          onRestore={onRestore}
-        />
-      ),
-    });
-  }
+		// Agregar la pestaña de archivados solo si el usuario actual es el propietario del perfil
+		const modifiedProfileData = { ...profileData, projects: deletedProjects }
+		tabs.push({
+			key: 'archived',
+			label: 'Archivados',
+			content: (
+				<ProfileProjects
+					tabName="Proyectos Archivados"
+					profileData={modifiedProfileData}
+					onRestore={onRestore}
+				/>
+			),
+		})
+	}
 
-  return (
-    <div className={styles.tabsContainer}>
-      <ul className={styles.tabs}>
-        {tabs.map((tab) => (
-          <li
-            key={tab.key}
-            className={`${styles.tab} ${
-              activeTab === tab.key ? 'bg-dark text-light' : ''
-            }`}
-            onClick={() => onClick(tab.key)} // Actualiza la URL y el estado de la pestaña activa
-          >
-            <a>{tab.label}</a>
-          </li>
-        ))}
-      </ul>
-      <div className={styles.tabContent}>
-        {tabs.map(
-          (tab) =>
-            activeTab === tab.key && (
-              <div key={tab.key} className={styles.projectsContainer}>
-                <div className={styles.titleContainer}>
-                  <h2>{tab.label}</h2>
-                  <div></div>
-                  {loggedUser.id === profileData?.id &&
-                    profileData?.planName !== 'Premium' &&
-                    profileData?.role !== 'admin' && (
-                      <Link to="/subscription" className={`${styles.button}`}>
-                        <button className="btn btn-light border">
-                          Actualizar a PRO
-                        </button>
-                      </Link>
-                    )}
-                </div>
-                {tab.content}
-              </div>
-            )
-        )}
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className={styles.tabsContainer}>
+			<ul className={styles.tabs}>
+				{tabs.map((tab) => (
+					<li
+						key={tab.key}
+						className={`${styles.tab} ${
+							activeTab === tab.key ? theme === "light" ? 'bg-dark text-light' : styles.exception : ''
+						}`}
+						onClick={() => setActiveTab(tab.key)}
+					>
+						<a>{tab.label}</a>
+					</li>
+				))}
+			</ul>
+			<div className={styles.tabContent}>
+				{tabs.map(
+					(tab) =>
+						activeTab === tab.key && (
+							<div key={tab.key} className={styles.projectsContainer}>
+								<div className={styles.titleContainer}>
+									<h2>{tab.label}</h2>
+									<div></div>
+									{loggedUser.id === profileData?.id &&
+										profileData?.planName !== 'Premium' &&
+										profileData?.role !== 'admin' && (
+											<Link to="/subscription" className={`${styles.button}`}>
+												<button className="btn btn-light border">
+													Actualizar a PRO
+												</button>
+											</Link>
+										)}
+								</div>
+								{tab.content}
+							</div>
+						)
+				)}
+			</div>
+		</div>
+	)
+}
 
 Tabs.propTypes = {
-  profileData: PropTypes.object.isRequired,
-  onRestore: PropTypes.func,
-  deletedProjects: PropTypes.array,
-  searchQuery: PropTypes.string,
-  activeContractTab: PropTypes.string,
-  handleReviewFormSubmit: PropTypes.func,
-  handleDelete: PropTypes.func,
-  onClick: PropTypes.func.isRequired, // Añadir propTypes para onClick
-  activeTab: PropTypes.string.isRequired, // Añadir propTypes para activeTab
-};
+	profileData: PropTypes.object.isRequired,
+	onRestore: PropTypes.func,
+	deletedProjects: PropTypes.array,
+}
 
-export default Tabs;
+export default Tabs
