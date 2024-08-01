99dac7dd84a9053d63b190fea076d5ee497e05bc
Mika <test@Tests-MacBook-Pro.local>
2024-08-01 04:47:26 -0300

    last last commit

--- src/components/reviews/Reviews.jsx
+++ src/components/reviews/Reviews.jsx
@@ -1,7 +1,6 @@
 import styles from './Reviews.module.css'
 import Modal from '../modal/Modal'
 import ReviewForm from './ReviewForm'
-import ProFooter from '../pro-footer/ProFooter'
 import { Link } from 'react-router-dom'
 import { useState } from 'react'
 import { CirclePlus, Star, X } from 'lucide-react'
@@ -14,58 +13,58 @@ const Reviews = ({ profileData, handleReviewFormSubmit, handleDelete }) => {
 
 	return (
 		<div className={styles.reviewsContainer}>
-			<div className={styles.cardsContainer}>
-				{profileData?.reviewsReceived?.length ? (
-					profileData?.reviewsReceived.map((review) => (
-						<Link key={review.id} className={styles.cardContentReview}>
-							<div className={styles.reviewerHeader}>
-								<div className={styles.reviewerImageContent}>
-									<img src={review.reviewer.image} alt={review.reviewer.id} />
-									<div className={styles.reviewUserNameContent}>
-										<h4 className="m-0 text-dark fw-bold fs-6">
-											{review.reviewer.userName}
-										</h4>
-										<span className="text-secondary">
-											{calculateTimeAgo(review.createdAt)}
-										</span>
-									</div>
-									<button
-										className="btn p-0"
-										onClick={() => handleDelete(review.id)}
-									>
+			{loggedUser?.id !== profileData.id && (
+				<div className={styles.cardsContainer}>
+					{profileData?.reviewsReceived?.length ? (
+						profileData?.reviewsReceived.map((review) => (
+							<Link key={review.id} className={styles.cardContentReview}>
+								<div className={styles.reviewerHeader}>
+									<div className={styles.reviewerImageContent}>
+										<img src={review.reviewer.image} alt={review.reviewer.id} />
+										<div className={styles.reviewUserNameContent}>
+											<h4 className="m-0 text-dark fw-bold fs-6">
+												{review.reviewer.userName}
+											</h4>
+											<span className="text-secondary">
+												{calculateTimeAgo(review.createdAt)}
+											</span>
+										</div>
 										{profileData.id === review.reviewedUser.id && (
-											<X
-												size={16}
-												strokeWidth={5}
-												color="#6c757d"
-												className="mb-4"
-											/>
+											<button
+												className="btn p-0"
+												onClick={() => handleDelete(review.id)}
+											>
+												<X
+													size={16}
+													strokeWidth={5}
+													color="#6c757d"
+													className="mb-4"
+												/>
+											</button>
 										)}
-									</button>
+									</div>
 								</div>
-							</div>
-							<div className={styles.reviewComment}>
-								{Array.from({ length: 5 }, (_, index) => (
-									<Star
-										key={index}
-										size={19}
-										strokeWidth={0}
-										fill={index < review.rating ? '#ffc107' : '#e4e5e9'}
-										style={{ cursor: 'pointer' }}
-									/>
-								))}
-								<div className={`${styles.commentContainer} text-dark`}>
-									<p>{review.comment}</p>
+								<div className={styles.reviewComment}>
+									{Array.from({ length: 5 }, (_, index) => (
+										<Star
+											key={index}
+											size={19}
+											strokeWidth={0}
+											fill={index < review.rating ? '#ffc107' : '#e4e5e9'}
+											style={{ cursor: 'pointer' }}
+										/>
+									))}
+									<div className={`${styles.commentContainer} text-dark`}>
+										<p>{review.comment}</p>
+									</div>
 								</div>
-							</div>
-						</Link>
-					))
-				) : loggedUser?.id === profileData.id ? (
-					<span>No hay reviews en el perfil</span>
-				) : null}
-				{loggedUser?.id !== profileData.id &&
-					profileData.role === 'admin' &&
-					profileData.planName === 'Premium' && (
+							</Link>
+						))
+					) : loggedUser?.id === profileData.id ? (
+						<span>No hay reviews en el perfil</span>
+					) : null}
+					{/* Mostrar el botón "Añadir review" solo para usuarios Premium */}
+					{loggedUser.planName === 'Premium' && (
 						<Link
 							className={styles.cardContent}
 							onClick={() => setShowModal(true)}
@@ -78,8 +77,8 @@ const Reviews = ({ profileData, handleReviewFormSubmit, handleDelete }) => {
 							</div>
 						</Link>
 					)}
-					{profileData.planName === 'Free' && <ProFooter />}
-			</div>
+				</div>
+			)}
 			<Modal
 				centered={true}
 				size="md"
